4356b9b5319149cc53008650d4ffc5fb73bc5c4a
evgeniishegai <evgeniishegai@yandex.ru>
2019-02-12 19:50:29 +0300

    C:\projects\job4j\chapter_001\src\main\java\ru>cd..

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,6 +1,18 @@
 package ru.job4j.calculate;
 
+/**
+*Calculate
+*
+*@author Evgenii Shegai (evgeniishegai@yandex.ru)
+*/
+
 public class Calculate {
+	
+	/**
+	* Method main
+	*@param [] args
+	*@return Hello my friend
+	*/
 	
 	public static void main(String[] args) {
 		System.out.println("Hello my friend");
---
+++ chapter_001/src/main/java/ru/job4j/calculate/package-info.java
@@ -0,0 +1,7 @@
+/**
+* Package for calculate task.
+*
+* @author Evgenii Shegai (evgeniishegai@yandex.ru)
+* @version $Id$
+* @since 0.1
+*/
