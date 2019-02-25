3be548094a0c870c4ab3d064ca62491f81ca5277
evgeniishegai <evgeniishegai@yandex.ru>
2019-02-25 20:17:39 +1000

    Пазлы. Тестовое задание.[#76937]

---
+++ puzzle/src/test/java/ru/job4j/puzzle/LogicTest.java
@@ -0,0 +1,67 @@
+package ru.job4j.puzzle;
+
+
+/** Test.
+ *
+ * @author EvgeniiShegai (evgeniishegai@yandex.ru)
+ * @version $Id$
+ * @since 0.1
+ */
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class LogicTest {
+
+    @Test
+    public void whenVerticalWin() {
+        Logic logic = new Logic(5) {
+            @Override
+            public int[][] convert() {
+                return new int[][] {
+                        {0, 0, 1, 0, 0},
+                        {0, 0, 1, 0, 0},
+                        {0, 0, 1, 0, 0},
+                        {0, 0, 1, 0, 0},
+                        {0, 0, 1, 0, 0},
+                };
+            }
+        };
+        assertThat(logic.isWin(), is(true));
+    }
+
+    @Test
+    public void whenHorizontalWin() {
+        Logic logic = new Logic(5) {
+            @Override
+            public int[][] convert() {
+                return new int[][] {
+                        {0, 0, 0, 0, 0},
+                        {0, 0, 0, 0, 0},
+                        {1, 1, 1, 1, 1},
+                        {0, 0, 0, 0, 0},
+                        {0, 0, 0, 0, 0},
+                };
+            }
+        };
+        assertThat(logic.isWin(), is(true));
+    }
+
+    @Test
+    public void whenNotWin() {
+        Logic logic = new Logic(5) {
+            @Override
+            public int[][] convert() {
+                return new int[][] {
+                        {0, 0, 1, 0, 0},
+                        {0, 0, 1, 0, 0},
+                        {1, 1, 0, 1, 1},
+                        {0, 0, 1, 0, 0},
+                        {0, 0, 1, 0, 0},
+                };
+            }
+        };
+        assertThat(logic.isWin(), is(false));
+    }
+}
