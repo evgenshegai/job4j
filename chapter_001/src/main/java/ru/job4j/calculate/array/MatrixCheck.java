9772bea63b2698094ae93df9c590ca86c5e7485e
evgeniishegai <evgeniishegai@yandex.ru>
2019-03-02 18:00:48 +1000

    Поправил класс MatrixCheck.java

--- chapter_001/src/main/java/ru/job4j/calculate/array/MatrixCheck.java
+++ chapter_001/src/main/java/ru/job4j/calculate/array/MatrixCheck.java
@@ -18,7 +18,7 @@ public class MatrixCheck {
     public boolean mono(boolean[][] data) {
         boolean result = true;
         int count = 0;
-        for (int i = 0; i < data.length - 1 ; i++) {
+        for (int i = 0; i < data.length - 1; i++) {
                 if ((data[i][i] != data[i + 1][i + 1]) || (data[i][data.length - i - 1] != (data[i + 1][data.length - i - 2])))  {
                     result = false;
                     break;
