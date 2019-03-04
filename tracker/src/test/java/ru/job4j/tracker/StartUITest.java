02cbcc9a558cc6ee4b3855ea733bcdc640be1d74
evgeniishegai <evgeniishegai@yandex.ru>
2019-03-04 21:15:30 +1000

    6. Доработать тесты в StartUITest проверяющие вывод данных в консоль.[#76967]

--- tracker/src/test/java/ru/job4j/tracker/StartUITest.java
+++ tracker/src/test/java/ru/job4j/tracker/StartUITest.java
@@ -11,22 +11,37 @@ package ru.job4j.tracker;
 import org.junit.Test;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
+import org.junit.After;
+import org.junit.Before;
 
 
 public class StartUITest {
 
+    private Tracker tracker;
+    private Item item;
+    private Item item2;
+
+    @Before
+    public void inStart() {
+        this.tracker = new Tracker();
+        this.item = new Item("test", "desc", 1);
+        this.item2 = new Item("test2", "desc2", 2);
+    }
+
+    @After
+    public void inFinish() {
+        System.out.println("The end");
+    }
+
     @Test
     public void whenUserAddItem() {
         Input input = new StubInput(new String[] {"0", "test", "desc", "1", "6"});
-        Tracker tracker = new Tracker();
         new StartUI(input, tracker).init();
         assertThat(tracker.findAll()[0].getName(), is("test"));
     }
 
     @Test
     public void whenAddItemThenUpdateItem() {
-        Tracker tracker = new Tracker();
-        Item item = new Item("test", "desc", 1);
         tracker.add(item);
         Input input = new StubInput(new String[] {"2", item.getId(), "test2", "desc2", "2", "6"});
         new StartUI(input, tracker).init();
@@ -35,9 +50,6 @@ public class StartUITest {
 
     @Test
     public void whenAddItemsThenFindItemById() {
-        Item item = new Item("test", "desc", 1);
-        Item item2 = new Item("test2", "desc2", 2);
-        Tracker tracker = new Tracker();
         tracker.add(item);
         Item temp = tracker.add(item2);
         Input input = new StubInput(new String[] {"4", temp.getId(), "6"});
@@ -47,14 +59,22 @@ public class StartUITest {
 
     @Test
     public void whenAddItemsThenDeleteOneItem() {
-        Item item = new Item("test", "desc", 1);
-        Item item2 = new Item("test2", "desc2", 2);
-        Tracker tracker = new Tracker();
         Item temp = tracker.add(item);
         tracker.add(item2);
         Input input = new StubInput(new String[] {"3", temp.getId(), "6"});
         new StartUI(input, tracker).init();
         assertThat(tracker.findAll()[0].getName(), is(item2.getName()));
+    }
+
+    @Test
+    public void whenAddItemsThenDisplayAll() {
+        tracker.add(item);
+        tracker.add(item2);
+        Input input = new StubInput(new String[] {"1", "6"});
+        new StartUI(input, tracker).init();
+        Item[] result = {item, item2};
+        assertThat(result, is(tracker.findAll()));
+
     }
 
 }
