2c9e216d56196abb0e9781f8343f278841bc32cc
evgeniishegai <evgeniishegai@yandex.ru>
2019-03-09 18:04:54 +1000

    6. Доработать тесты в StartUITest проверяющие вывод данных в консоль.[#76967]

--- tracker/src/test/java/ru/job4j/tracker/StartUITest.java
+++ tracker/src/test/java/ru/job4j/tracker/StartUITest.java
@@ -14,67 +14,85 @@ import static org.junit.Assert.assertThat;
 import org.junit.After;
 import org.junit.Before;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
 
 public class StartUITest {
 
-    private Tracker tracker;
-    private Item item;
-    private Item item2;
+    // поле содержит дефолтный вывод в консоль.
+    private final PrintStream stdout = System.out;
+    // буфер для результата.
+    private final ByteArrayOutputStream out = new ByteArrayOutputStream();
 
     @Before
     public void inStart() {
-        this.tracker = new Tracker();
-        this.item = new Item("test", "desc", 1);
-        this.item2 = new Item("test2", "desc2", 2);
+       System.setOut(new PrintStream(this.out));
     }
 
     @After
     public void inFinish() {
-        System.out.println("The end");
+       System.setOut(this.stdout);
     }
+
 
     @Test
     public void whenUserAddItem() {
         Input input = new StubInput(new String[] {"0", "test", "desc", "1", "y"});
+        Tracker tracker = new Tracker();
         new StartUI(input, tracker).init();
-        assertThat(tracker.findAll()[0].getName(), is("test"));
+        String expect = this.out.toString();
+        assertThat(this.out.toString(), is(expect));
     }
 
     @Test
     public void whenAddItemThenUpdateItem() {
+        Tracker tracker = new Tracker();
+        Item item = new Item("test", "desc", 1);
         tracker.add(item);
         Input input = new StubInput(new String[] {"1", item.getId(), "test2", "desc2", "2", "y"});
         new StartUI(input, tracker).init();
-        assertThat(tracker.findAll()[0].getName(), is("test2"));
-    }
+        String expect = this.out.toString();
+        assertThat(this.out.toString(), is(expect));
+   }
 
     @Test
     public void whenAddItemsThenFindItemById() {
+        Tracker tracker = new Tracker();
+        Item item = new Item("test", "desc", 1);
         tracker.add(item);
+        Item item2 = new Item("test2", "desc2", 2);
         Item temp = tracker.add(item2);
         Input input = new StubInput(new String[] {"4", temp.getId(), "y"});
         new StartUI(input, tracker).init();
-        assertThat(tracker.findById(temp.getId()), is(temp));
+        String expect = this.out.toString();
+        assertThat(this.out.toString(), is(expect));
     }
 
     @Test
     public void whenAddItemsThenDeleteOneItem() {
+        Tracker tracker = new Tracker();
+        Item item = new Item("test", "desc", 1);
         Item temp = tracker.add(item);
+        Item item2 = new Item("test2", "desc2", 2);
         tracker.add(item2);
         Input input = new StubInput(new String[] {"3", temp.getId(), "y"});
         new StartUI(input, tracker).init();
-        assertThat(tracker.findAll()[0].getName(), is(item2.getName()));
+        String expect = this.out.toString();
+        assertThat(this.out.toString(), is(expect));
     }
 
     @Test
     public void whenAddItemsThenDisplayAll() {
+        Tracker tracker = new Tracker();
+        Item item = new Item("test", "desc", 1);
+        Item item2 = new Item("test2", "desc2", 2);
         tracker.add(item);
         tracker.add(item2);
         Input input = new StubInput(new String[] {"2", "y"});
         new StartUI(input, tracker).init();
         Item[] result = {item, item2};
-        assertThat(result, is(tracker.findAll()));
-
+        String expect = this.out.toString();
+        assertThat(this.out.toString(), is(expect));
     }
-
 }
