0a80daee8fcb8961fd4a53e388ba9e97bd0dc7be
evgeniishegai <evgeniishegai@yandex.ru>
2019-03-28 21:23:45 +1000

    Банковские переводы.[#76951]

---
+++ chapter_003/src/main/java/ru/job4j/collections/User3.java
@@ -0,0 +1,41 @@
+package ru.job4j.collections;
+
+/**
+ * class User3
+ * @author Shegai Evgenii
+ */
+
+import java.util.Objects;
+
+public class User3 {
+
+    private String name;
+    private int passport;
+
+    public User3(String name, int passport) {
+        this.name = name;
+        this.passport = passport;
+    }
+
+
+    public int getPassport() {
+        return passport;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        User3 user3 = (User3) o;
+        return passport == user3.passport && Objects.equals(name, user3.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, passport);
+    }
+}
