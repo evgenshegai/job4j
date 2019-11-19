270c0d859a17a7edcf99b58f434d624686c7890c
evgenshegai <evgeniishegai@yandex.ru>
2019-11-19 21:25:18 +1000

    6.7. Квадратный массив заполнен true или false по диагоналям.[#76913] новая задача

--- chapter_001/src/main/java/ru/job4j/calculate/array/MatrixCheck.java
+++ chapter_001/src/main/java/ru/job4j/calculate/array/MatrixCheck.java
@@ -1,7 +1,8 @@
 package ru.job4j.calculate.array;
 
 /**
- * Квадратный массив заполнен true or false по диагоналям
+ * Квадратный массив заполнен true or false по диагоналям 1 решение
+ * Квадратный массив заполнен крестиками правильно или нет 2 решение
  * @author Evgenii Shegay(evgeniishegai@yandex.ru)
  *  @version $Id$
  *  @since 0.1
@@ -9,15 +10,9 @@ package ru.job4j.calculate.array;
 
 public class MatrixCheck {
 
-    /**
-     * ПРоверяем массив по диагоналям
-     * @param data вхоящий массив
-     * @return будево значение
-     */
 
-    public boolean mono(boolean[][] data) {
+   /* public boolean mono(boolean[][] data) {
         boolean result = true;
-        int count = 0;
         for (int i = 0; i < data.length - 1; i++) {
                 if ((data[i][i] != data[i + 1][i + 1]) || (data[i][data.length - i - 1] != (data[i + 1][data.length - i - 2])))  {
                     result = false;
@@ -25,5 +20,42 @@ public class MatrixCheck {
                 }
         }
         return result;
+    }*/
+
+    /**
+     * метод определяет выиграл пользователь или нет
+     * @param board массив символов
+     * @return true or false смотря какой результат
+     */
+    public  static boolean isWin(char[][] board) {
+        boolean result = false;
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board.length; j++) {
+                char sign = board[i][j];
+                int count = 0;
+                  if (sign == 'X' && i == 0) {
+                      while (i < board.length - 1) {
+
+                          if (sign == board[i++][j]) {
+                              count++;
+                          }
+                          if (count == 4) {
+                              result = true;
+                          }
+                      }
+                      } else if (sign == 'X' && j == 0) {
+                      while (j < board.length - 1) {
+                          if (sign == board[i][j++]) {
+                              count++;
+                          }
+                          if (count == 4) {
+                              result = true;
+                          }
+                      }
+                  }
+                  }
+                }
+            return result;
+        }
     }
-}
+
--- chapter_001/src/test/java/ru/job4j/calculate/array/MatrixCheckTest.java
+++ chapter_001/src/test/java/ru/job4j/calculate/array/MatrixCheckTest.java
@@ -10,11 +10,12 @@ package ru.job4j.calculate.array;
 import org.junit.Test;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
+import static ru.job4j.calculate.array.MatrixCheck.isWin;
 
 
 public class MatrixCheckTest {
 
-    @Test
+  /*  @Test
     public void whenDataTrue() {
         MatrixCheck matrixCheck = new MatrixCheck();
         boolean[][] array = {{false, true, false},
@@ -39,11 +40,51 @@ public class MatrixCheckTest {
         MatrixCheck check = new MatrixCheck();
         boolean[][] input = new boolean[][] {
                 {true, true, true, true},
-                {true, false, true, true},
                 {true, true, true, true},
-                {true, true, true, true}
+                {true, true, true, true},
+                {false, true, true, true}
         };
         boolean result = check.mono(input);
         assertThat(result, is(false));
+    }*/
+
+
+  @Test
+    public void test() {
+      char[][] hasWinVertical = {
+              {'_', '_', 'X', '_', '_'},
+              {'_', '_', 'X', '_', '_'},
+              {'_', '_', 'X', '_', '_'},
+              {'_', '_', 'X', '_', '_'},
+              {'_', '_', 'X', '_', '_'},
+      };
+      boolean win = isWin(hasWinVertical);
+      assertThat(win, is(true));
+  }
+
+  @Test
+          public void test2() {
+      char[][] hasWinHor = {
+              {'_', '_', '_', '_', '_'},
+              {'X', 'X', 'X', 'X', 'X'},
+              {'_', '_', '_', '_', '_'},
+              {'_', '_', '_', '_', '_'},
+              {'_', '_', '_', '_', '_'},
+      };
+      boolean winHor = isWin(hasWinHor);
+     assertThat(winHor, is(true));
+  }
+
+  @Test
+  public void test3() {
+      char[][] notWin = {
+              {'_', '_', 'X', '_', '_'},
+              {'_', '_', 'X', '_', '_'},
+              {'_', 'X', '_', '_', '_'},
+              {'_', '_', 'X', '_', '_'},
+              {'_', '_', 'X', '_', '_'},
+      };
+      boolean lose = isWin(notWin);
+      assertThat(lose, is(false));
     }
 }
